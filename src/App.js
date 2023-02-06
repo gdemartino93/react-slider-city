3104deb0352cd1901fa8c8b02d276d5f59290105
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-07 00:42:33 +0100

    create title component with props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-
+import Title from './components/Title';
 function App() {
   return (
     <div className="App">
-
+      <header>
+        <Title name="Le mete preferite"/>
+      </header>
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = ({name}) => {
+  return (
+    <div className='d-flex justify-content-center'>
+        <h1>{name}</h1>
+    </div>
+  )
+}
+
+export default Title
