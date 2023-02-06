ad6167094fbf3bede05927cb42274ac844fc79b9
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-07 00:44:47 +0100

    title props empty = titolo

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Title = ({name}) => {
   return (
     <div className='d-flex justify-content-center'>
-        <h1>{name}</h1>
+        <h1>{name || "Titolo"}</h1>
     </div>
   )
 }
