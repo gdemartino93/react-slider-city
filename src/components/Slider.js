bd6fc5923c7c8b670c46347279c1a6370777cf56
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-07 01:25:08 +0100

    i axios, created and hosted json to call as api

---
+++ src/components/Slider.js
@@ -0,0 +1,12 @@
+import React, {useState , useEffect} from 'react';
+import axios from 'axios';
+// I've created json file and hosted on https://jsonbin.io as public
+const url = "https://api.jsonbin.io/v3/b/63e199d4ace6f33a22d80fe4";
+
+const Slider = () => {
+  return (
+    <div>Slider</div>
+  )
+}
+
+export default Slider
